ca96ccebabe7aee80d4913ba0a315c360cb00686
Marcelo Lacerda <marcelolljp@gmail.com>
2021-06-26 16:52:29 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/mlac/cursomc/services/ClienteService.java
+++ src/main/java/com/mlac/cursomc/services/ClienteService.java
@@ -15,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository catre;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = catre.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
