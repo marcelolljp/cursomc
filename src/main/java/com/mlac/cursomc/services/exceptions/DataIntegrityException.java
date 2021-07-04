66f68f67fa6fd6cfb3392460d87652346a2a11dd
Marcelo Lacerda <marcelolljp@gmail.com>
2021-07-03 22:51:42 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/mlac/cursomc/services/exceptions/DataIntegrityException.java
+++ src/main/java/com/mlac/cursomc/services/exceptions/DataIntegrityException.java
@@ -1,7 +1,6 @@
 package com.mlac.cursomc.services.exceptions;
 
 public class DataIntegrityException extends RuntimeException{
-
 	private static final long serialVersionUID = 1L;
 	
 	public DataIntegrityException(String msg) {
--- src/main/java/com/mlac/cursomc/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/mlac/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -1,7 +1,6 @@
 package com.mlac.cursomc.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException{
-
 	private static final long serialVersionUID = 1L;
 	
 	public ObjectNotFoundException(String msg) {
