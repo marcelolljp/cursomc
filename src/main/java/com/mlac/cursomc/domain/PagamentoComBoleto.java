249c7b3aeb34cb2907aae2a8104bfd3b23931947
Marcelo Lacerda <marcelolljp@gmail.com>
2021-07-15 00:19:17 -0300

    Inserindo Pedido

--- src/main/java/com/mlac/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/mlac/cursomc/domain/PagamentoComBoleto.java
@@ -5,9 +5,11 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.mlac.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
@@ -17,7 +19,7 @@ public class PagamentoComBoleto extends Pagamento{
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 	
-	public PagamentoComBoleto( ) {
+	public PagamentoComBoleto() {
 	}
 
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
